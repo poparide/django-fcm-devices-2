6800f867be12837dc4f653bd5dfba0ef62411ef2
James Priebe <jamespriebe1@gmail.com>
2024-08-21 12:23:37 -0400

    cleanup

--- fcm_devices/settings.py
+++ fcm_devices/settings.py
@@ -13,18 +13,12 @@ class AppSettings(object):
 
 
 DEFAULTS = {
-    # firebase credentials file, found at
+    # from firebase service account file, found at
     # https://console.firebase.google.com/u/0/project/_/settings/serviceaccounts/adminsdk
-    # need to figure out how to load this... is it a path or a file?
-    # it's loaded via google.oauth2.service_account.Credentials.from_service_account_file
-    # https://googleapis.dev/python/google-auth/latest/reference/google.oauth2.service_account.html#module-google.oauth2.service_account
-    # so we can also do from_service_account_info if we have it stored, which may be preferable...
-    # and in PyFCM we can pass credentials instead of service_account_file,
-    # so maybe just want to do that instead (pass in FCM credentials from our own project settings)
-    # Loaded in Credentials and sent off to backend.
     "GOOGLE_SERVICE_ACCOUNT_INFO": None,
-     # this is redundant, as it can be inferred from credentials/account info,
-     # but alas is a required param for PyFCM at the moment
+    # this is redundant, as it can be inferred from credentials/account info,
+    # but alas is a required param for PyFCM at the moment.
+    # See https://github.com/olucurious/PyFCM/issues/357
     "GOOGLE_PROJECT_ID": None,
 
     # allow customisation of how messages are actually sent
